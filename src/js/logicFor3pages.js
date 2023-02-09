62c0a5883183cf6b98f1da9f4ad2a1bfc6aa16dd
teiatan <92535482+teiatan@users.noreply.github.com>
2023-02-09 23:49:31 +0200

    3 pages

--- src/js/logicFor3pages.js
+++ src/js/logicFor3pages.js
@@ -1,10 +1,13 @@
+//заповнення локального сховища значеннями за замовчуванням
 export function fillLocalStorageIfEmpty() {
-    console.log(localStorage);
     if(localStorage.length === 0) {
         localStorage.lang = 'en';
+        localStorage.theme = 'light';
         localStorage.auth = 'no';
+        localStorage.favorite = [];
+        localStorage.read = [];
     };
-}
+};
 
 
 
