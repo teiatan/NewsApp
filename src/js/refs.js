5967ba5b2e80f91c94a37eb20cc54f52440abe74
teiatan <92535482+teiatan@users.noreply.github.com>
2023-02-11 23:59:29 +0200

    Create refs.js

---
+++ src/js/refs.js
@@ -0,0 +1,9 @@
+export const refs = {
+    // АВТОРИЗАЦІЯ
+
+  authEmailInput: document.querySelector('.js-auth__email-input'),
+  authPasswordInput: document.querySelector('.js-auth__password-input'),
+  authEntranceBtn: document.querySelector('.js-auth__entrance-btn'),
+  authRegistrationBtn: document.querySelector('.js-auth__registration-btn'),
+
+};
